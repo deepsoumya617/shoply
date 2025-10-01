169dbd60290a34f1a563464fff87aa486da8d4b7
deepsoumya617 <soumyadeepghosh617@gmail.com>
2025-10-01 20:19:06 +0530

    feat(session): integrate Redis session store and add session management

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,35 @@ import express, { Express } from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
+import { RedisStore } from 'connect-redis'
+import session from 'express-session'
 import authRoutes from './modules/auth/auth.routes'
 import productRoutes from './modules/product/product.routes'
 import userRoutes from './modules/user/user.routes'
+import redis from './config/redis'
+import { env } from './config/env'
 
 const app: Express = express()
+
+// redis store setup
+const redisStore = new RedisStore({
+  client: redis,
+  prefix: 'sess:',
+})
+
+app.use(
+  session({
+    store: redisStore,
+    secret: env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      sameSite: 'lax',
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+)
 
 // middleware setup
 app.use(cors())

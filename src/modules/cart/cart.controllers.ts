d45aeffaed24a74b308a1fdf01ca76eb23a57c9e
deepsoumya617 <soumyadeepghosh617@gmail.com>
2025-10-04 11:46:56 +0530

    feat(cart): implement getCartItems function to fetch cart details and items

--- src/modules/cart/cart.controllers.ts
+++ src/modules/cart/cart.controllers.ts
@@ -5,6 +5,7 @@ import { db } from '../../config/db'
 import { carts } from '../../db/schema/cart'
 import { eq, sql } from 'drizzle-orm'
 import { cartItems } from '../../db/schema/cartItems'
+import { products } from '../../db/schema'
 
 export async function addItems(req: AuthRequest, res: Response) {
   const result = addItemToCartSchema.safeParse(req.body)
@@ -17,7 +18,7 @@ export async function addItems(req: AuthRequest, res: Response) {
       message: 'Invalid input data. Please check and try again.',
     })
   }
-  
+
   const { productId, quantity } = result.data
 
   try {
@@ -54,4 +55,58 @@ export async function addItems(req: AuthRequest, res: Response) {
   }
 }
 
-export async function getCartItems(req: AuthRequest, res: Response) {}
+export async function getCartItems(req: AuthRequest, res: Response) {
+  try {
+    // first fetch the cart
+    const [cart] = await db
+      .select()
+      .from(carts)
+      .where(eq(carts.userId, req.user!.userId))
+
+    // maybe cart is empty
+    if (!cart) {
+      return res.json({
+        success: true,
+        cart: {
+          id: null,
+          items: [],
+          totalPrice: 0,
+        },
+      })
+    }
+
+    // fetch cart items w. product details -> name desc price
+    const items = await db
+      .select({
+        productId: cartItems.productId,
+        quantity: cartItems.quantity,
+        name: products.name,
+        description: products.description,
+        price: products.price,
+      })
+      .from(cartItems)
+      .innerJoin(products, eq(cartItems.productId, products.id))
+      .where(eq(cartItems.cartId, cart.id))
+
+    // calculate total
+    const itemsWSubtotal = items.map(item => ({
+      ...item,
+      subtotal: item.quantity * item.price,
+    }))
+
+    const totalPrice = itemsWSubtotal.reduce((sum, i) => sum + i.subtotal, 0)
+
+    // send response
+    res.status(200).json({
+      success: true,
+      cart: {
+        id: cart.id,
+        items,
+        totalPrice,
+      },
+    })
+  } catch (error) {
+    console.error('Failed to fetch cart items: ', error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+}
